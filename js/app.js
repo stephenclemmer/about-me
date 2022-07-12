2202a40cf40f578c92ebe65abe116b8d9cb8d82d
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-07-12 16:27:33 -0700

    Commented out console logs

--- js/app.js
+++ js/app.js
@@ -1,19 +1,18 @@
 'use strict';
 
 let userName = prompt('Please type your preferred name');
-console.log(userName);
+// console.log(userName);
 alert(`Hello ${userName}! It's great to meet you! Please continue to play a short guessing game about Stephen Clemmer`);
 
 let questionOne = prompt('Answer "yes" or "no": Does Stephen Clemmer love coding?').toLowerCase();
-console.log(questionOne);
-
+// console.log(questionOne);
 if (questionOne === 'yes' || questionOne === 'y'){alert(`That's right ${userName}!, Stephen Clemmer loves coding because it allows him to work on interesting problems and meet interesting people!`);
 } else {
   alert(`I'm sorry, ${userName}, but you're incorrect. Stephen Clemmer LOVES to code!`);
 }
 
 let questionTwo = prompt('Answer "yes" or "no": Has Stephen worked with Grammy Award winning artists?').toLowerCase();
-console.log(questionTwo);
+// console.log(questionTwo);
 if (questionTwo === 'yes' || questionTwo === 'y'){
   alert(`That's right ${userName}! Stephen served as an engineer for the Rolling Stones' remastered recordings of their 1969 album Get Yer YaYa's Out, recorded live at Madison Square Garden. Stephen also recorded Norah Jones' live set which was featured on her album The Fall.`);
 } else {
@@ -21,7 +20,7 @@ if (questionTwo === 'yes' || questionTwo === 'y'){
 }
 
 let questionThree = prompt('Answer "yes" or "no": Is Stephen a pest control expert?').toLowerCase();
-console.log(questionThree);
+// console.log(questionThree);
 if (questionThree === 'yes' || questionThree === 'y'){
   alert(`That's right ${userName}! Stephen is an expert in insect and rodent pest control. He used this knowledge to solve decades-long pest infestations for the City of Chicago's public buildings, schools, airpots, and public transit. He's proud to have been able to affect so many lives by making that city a safer, greener place to live!`);
 } else {
@@ -29,7 +28,7 @@ if (questionThree === 'yes' || questionThree === 'y'){
 }
 
 let questionFour = prompt('Answer "yes" or "no": Does Stephen love to cook and eat?').toLowerCase();
-console.log(questionFour);
+// console.log(questionFour);
 if (questionFour === 'yes' || questionFour === 'y'){
   alert(`That's right ${userName}! The only thing Stephen loves more than cooking food is eating it. His favorite thing to make is lasagne because he can pack a ton of flavor into it. If you ask nicely, he'll give you the recipe!`);
 } else {
@@ -37,7 +36,7 @@ if (questionFour === 'yes' || questionFour === 'y'){
 }
 
 let questionFive = prompt('Answer "yes" or "no": Does Stephen like playing soccer?').toLowerCase();
-console.log(questionFive);
+// console.log(questionFive);
 if (questionFive === 'no' || questionFive === 'n'){
   alert(`That's right ${userName}! Stephen does not enjoy playing soccer. He'd rather play guitar or piano, or go to a yoga class`);
 } else {
