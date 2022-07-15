77152ae5968588319350d561c83c23469202fd76
Stephen Clemmer <stephenjclemmer@gmail.com>
2022-07-14 22:22:01 -0700

    created guessing game with array for question 7 and created a scorekeeper for all questions

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 'use strict';
 
 let userName = prompt('Please type your preferred name');
-  // console.log(userName);
+// console.log(userName);
 
-  alert(`Hello ${userName}! It's great to meet you! Please continue to play a short guessing game about Stephen Clemmer`);
+alert(`Hello ${userName}! It's great to meet you! Please continue to play a short guessing game about Stephen Clemmer`);
 
+let score = 0;
 
 
 firstQuestion();
@@ -13,6 +14,7 @@ thirdQuestion();
 fourthQuestion();
 fifthQuestion();
 sixthQuestion();
+seventhQuestion();
 
 
 
@@ -21,10 +23,11 @@ function firstQuestion(){
   let questionOne = prompt('Answer "yes" or "no": Does Stephen Clemmer love coding?').toLowerCase();
 
   if (questionOne === 'yes' || questionOne === 'y'){alert(`That's right ${userName}!, Stephen Clemmer loves coding because it allows him to work on interesting problems and meet interesting people!`);
+    score++;
   } else {
     alert(`I'm sorry, ${userName}, but you're incorrect. Stephen Clemmer LOVES to code!`);
   }
-  
+  alert(`Your score is ${score} out of 1.`);
 }
 
 function secondQuestion(){
@@ -32,9 +35,11 @@ function secondQuestion(){
   
   if (questionTwo === 'yes' || questionTwo === 'y'){
     alert(`That's right ${userName}! Stephen served as an engineer for the Rolling Stones' remastered recordings of their 1969 album Get Yer YaYa's Out, recorded live at Madison Square Garden. Stephen also recorded Norah Jones' live set which was featured on her album The Fall.`);
+    score++;
   } else {
     alert(`Sorry, ${userName} but Stephen has been very fortunate. He served as an engineer for the Rolling Stones' remastered recordings of their 1969 album Get Yer YaYa's Out, recorded live at Madison Square Garden. Stephen also recorded Norah Jones' live set which was featured on her album The Fall.`);
   }
+  alert(`Your score is ${score} out of 2.`);
 }
 
 function thirdQuestion(){
@@ -42,9 +47,11 @@ function thirdQuestion(){
   // console.log(questionThree);
   if (questionThree === 'yes' || questionThree === 'y'){
     alert(`That's right ${userName}! Stephen is an expert in insect and rodent pest control. He used this knowledge to solve decades-long pest infestations for the City of Chicago's public buildings, schools, airpots, and public transit. He's proud to have been able to affect so many lives by making that city a safer, greener place to live!`);
+    score++
   } else {
     alert(`Sorry, ${userName} but Stephen is an insect and rodent pest control expert. He used this knowledge to solve decades-long pest infestations for the City of Chicago's public buildings, schools, airpots, and public transit. He's proud to have been able to affect so many lives by making that city a safer, greener place to live!`);
   }
+  alert(`Your score is ${score} out of 3.`);
 }
 
 function fourthQuestion(){
@@ -52,9 +59,11 @@ function fourthQuestion(){
   // console.log(questionFour);
   if (questionFour === 'yes' || questionFour === 'y'){
     alert(`That's right ${userName}! The only thing Stephen loves more than cooking food is eating it. His favorite thing to make is lasagne because he can pack a ton of flavor into it. If you ask nicely, he'll give you the recipe!`);
+    score++;
   } else {
     alert(`Sorry, ${userName} but the only thing Stephen loves more than cooking is eating. His favorite thing to make is lasagne because he can pack a ton of flavor into it. If you ask nicely, he'll give you the recipe!`);
   }
+  alert(`Your score is ${score} out of 4.`);
 }
 
 function fifthQuestion(){
@@ -62,25 +71,28 @@ function fifthQuestion(){
   // console.log(questionFive);
   if (questionFive === 'no' || questionFive === 'n'){
     alert(`That's right ${userName}! Stephen does not enjoy playing soccer. He'd rather play guitar or piano, or go to a yoga class`);
+    score++;
   } else {
     alert(`Sorry, ${userName} but Stephen does not enjoy playing soccer. He'd rather play guitar or piano, or go to a yoga class`);
   }
+  alert(`Your score is ${score} out of 5.`);
 }
 
 // Start of Lab 03
 // https://www.codecademy.com/forum_questions/5575df34937676a0b50000e8#:~:text=Answer%20557617aae0a300f2cf000609&text=prompt()%20always%20returns%20a,'string'%20and%20not%20null%20.
 
+
 function sixthQuestion(){
 // Define a variable for the maximum number of guesses (4)
-let guessLimit = 4;
+  let guessLimit = 4;
 
-// Define the variable for your favorite number. (12)
-let favNum = '12';
+  // Define the variable for your favorite number. (12)
+  let favNum = '12';
 
-// Define a variable for wrong guesses called correctNumber that is False so that you can set up a while loop to end the game when either guesses have run out, or when the faveNum is correctly guessed.
-let correctNumber = false;
+  // Define a variable for wrong guesses called correctNumber that is False so that you can set up a while loop to end the game when either guesses have run out, or when the faveNum is correctly guessed.
+  let correctNumber = false;
 
-let questionSix;
+  let questionSix;
 
 
   while(guessLimit > 0 && correctNumber === false){
@@ -91,8 +103,11 @@ let questionSix;
     } else if (questionSix > 12){
       questionSix = prompt('Too high. What\'s you next guess?');
     } else if (questionSix === '12'){
+      // totalScore++
       alert('Stephen\'s favorite number is twelve because it is so musical. Twelve can be subdivided as 1, 2, 3, 4, 6, and 12! TONS OF FUN!');
       correctNumber = true;
+      score++;
+
     }
     guessLimit--;
   }
@@ -100,6 +115,7 @@ let questionSix;
   if(guessLimit === 0){
     alert('Nice try. Here\'s the answer. Stephen\'s favorite number is twelve because it is so musical. Twelve can be subdivided as 1, 2, 3, 4, 6, and 12! TONS OF FUN!');
   }
+  alert(`Your score is ${score} out of 6.`);
 }
 
 // 1. correctNumber = true
@@ -112,5 +128,26 @@ let questionSix;
 // let guessNum = 4
 
 
-// Question Seven for Lab 3
+// // Question Seven for Lab 3
 
+function seventhQuestion(){
+// Make array with possible favorite animal answers
+  let favAnimal = ['tiger', 'dog', 'rat', 'meerkat'];
+
+  // Give six attempts to answer
+
+  for(let i = 0; i < 6; i++){
+    let questionSeven = prompt(`${userName}, can you guess one of Stephen's favorite animals in six guesses or less?`).toLowerCase();
+
+    for(let j = 0; j < favAnimal.length; j++){
+      if(questionSeven === favAnimal[j]){
+        alert('You got it! I love that animal!');
+        score++;
+        i = 6;
+        break;
+      }
+    }
+
+  } alert (`These were all of the possible guesses ${favAnimal}`);
+  alert(`Your score is ${score} out of 7.`);
+}
