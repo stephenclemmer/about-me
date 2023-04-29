7e93bd09ff48f4123411450bb6860699fe03ef5b
Stephen Clemmer <stephenjclemmer@gmail.com>
2023-04-29 15:37:36 -0700

    made improvements in CSS

--- js/app.js
+++ js/app.js
@@ -2,18 +2,12 @@
 
 let userName = prompt('Please type your preferred name');
 
-alert(`Hello ${userName}! It's great to meet you! Please continue to play a short guessing game about Stephen Clemmer`);
+alert(`Hello ${userName}! I'm Stephen. It's great to meet you! Please play a short guessing game about me`);
 
 let score = 0;
 
 
-firstQuestion();
-secondQuestion();
-thirdQuestion();
-fourthQuestion();
-fifthQuestion();
-sixthQuestion();
-seventhQuestion();
+
 
 
 function firstQuestion(){
@@ -22,7 +16,7 @@ function firstQuestion(){
   if (questionOne === 'yes' || questionOne === 'y'){alert(`That's right ${userName}!, I love coding because it is full of possibilities`);
     score++;
   } else {
-    alert(`I'm sorry, ${userName}, but you're incorrect. Stephen Clemmer LOVES to code!`);
+    alert(`I'm sorry, ${userName}, but you're incorrect. I love to code!`);
   }
   alert(`Your score is ${score} out of 1.`);
 }
@@ -32,7 +26,7 @@ function secondQuestion(){
   let questionTwo = prompt('Answer "yes" or "no": Have I worked with Grammy Award winning artists?').toLowerCase();
 
   if (questionTwo === 'yes' || questionTwo === 'y'){
-    alert(`That's right ${userName}! I had a fun gig in mhy 20's. I served as an engineer for the Rolling Stones' remastered recordings of their 1969 album Get Yer YaYa's Out, recorded live at Madison Square Garden. I also recorded Norah Jones' featured on a special release of her album, The Fall.`);
+    alert(`That's right ${userName}! I had a fun gig in mhy 20's. I served as an engineer for the Rolling Stones' remastered recordings of their 1969 album Get Yer YaYa's Out, recorded live at Madison Square Garden. I also recorded for Norah Jones' album, The Fall.`);
     score++;
   } else {
     alert(`Sorry, ${userName} but I had a cool gig in my 20's. I served as an engineer for the Rolling Stones' remastered recordings of their 1969 album Get Yer YaYa's Out, recorded live at Madison Square Garden. I also recorded Norah Jones' featured on a special release of her album, The Fall.`);
@@ -68,7 +62,7 @@ function fourthQuestion(){
 
 
 function fifthQuestion(){
-  let questionFive = prompt('Answer "yes" or "no": Does Stephen like playing soccer?').toLowerCase();
+  let questionFive = prompt('Answer "yes" or "no": Do I like playing soccer?').toLowerCase();
 
   if (questionFive === 'no' || questionFive === 'n'){
     alert(`That's right ${userName}! I do not enjoy playing soccer. I'd rather play guitar or piano, or go to a yoga class`);
@@ -94,7 +88,7 @@ function sixthQuestion(){
     } else if (questionSix > 12){
       questionSix = prompt('Too high. What\'s you next guess?');
     } else if (questionSix === '12'){
-      alert('Stephen\'s favorite number is twelve because it is so musical. Twelve can be subdivided as 1, 2, 3, 4, 6, and 12! TONS OF FUN!');
+      alert('My favorite number is twelve because it is so musical. Twelve can be subdivided as 1, 2, 3, 4, 6, and 12! TONS OF FUN!');
       correctNumber = true;
       score++;
     }
@@ -113,7 +107,7 @@ function seventhQuestion(){
 
 
   for(let i = 0; i < 6; i++){
-    let questionSeven = prompt(`${userName}, can you guess one of Stephen's favorite animals in six guesses or less?`).toLowerCase();
+    let questionSeven = prompt(`${userName}, can you guess one of my favorite animals in six guesses or less?`).toLowerCase();
 
     for(let j = 0; j < favAnimal.length; j++){
       if(questionSeven === favAnimal[j]){
@@ -123,6 +117,14 @@ function seventhQuestion(){
         break;
       }
     }
-  } alert (`These were all of the possible guesses ${favAnimal}`);
+  } alert (`These were all of the possible guesses: ${favAnimal}`);
   alert(`Your score is ${score} out of 7.`);
 }
+
+firstQuestion();
+secondQuestion();
+thirdQuestion();
+fourthQuestion();
+fifthQuestion();
+sixthQuestion();
+seventhQuestion();
